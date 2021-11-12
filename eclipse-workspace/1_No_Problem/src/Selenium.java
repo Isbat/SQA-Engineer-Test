d7b983e25e14de3cac86166eb57b56d666f1a71e
Isbat Ahmed <36653987+Isbat@users.noreply.github.com>
2021-11-12 12:56:18 +0600

    visit url phptravels

--- eclipse-workspace/1_No_Problem/src/Selenium.java
+++ eclipse-workspace/1_No_Problem/src/Selenium.java
@@ -1,3 +1,6 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Selenium {
 
@@ -5,6 +8,11 @@ public class Selenium {
 		// TODO Auto-generated method stub
 		
 		System.setProperty("webdriver.chrome.driver", "E:\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.phptravels.net");
+		driver.findElement(By.className("select2-search__field")).sendKeys("Johor Bahru");
+		
+		
 		
 
 	}
